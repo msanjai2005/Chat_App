080adb669397f537f37833682fd67098d3dfc576
msanjai2005 <msanjai1012@gmail.com>
2025-12-24 12:24:17 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials:true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders:["Content-Type","Authorization"]
